1e511b0a08d5fc4ad11728523472f5962c12399a
Keith Green <kegreen22@gmail.com>
2016-02-26 23:04:25 -0500

    initial commit of game with skeleton methods

---
+++ chess.rb
@@ -0,0 +1,27 @@
+class Player
+# attributes - move, name, icon, status
+  attr_accessor :name, :icon, :status
+
+end
+
+class Match
+  attr_accessor :board, :game_over, :winner
+
+end
+
+def turn(player_name, player_icon, game_over)
+  end
+
+def victory_check(player_name, player_icon, game_over)
+  end
+
+def show_board   # show the game board
+
+end
+
+def show_game_turn(game_turn)   # show the game turn
+    puts game_turn
+end
+
+# game_flow - you_go/I_go movement
+
